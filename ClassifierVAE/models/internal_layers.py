a3072f0d2c98ef5f81b4899736d0ae0af3be6927
Andrew Parry <2393265p@student.gla.ac.uk>
2022-10-24 13:03:12 +0100

    Updating internal architecture for efficiency

--- ClassifierVAE/models/internal_layers.py
+++ ClassifierVAE/models/internal_layers.py
@@ -17,28 +17,27 @@ returns:
 
 def init_convnet(stack, kernel_size=5, strides=(2, 2), activation='relu', dropout_rate=None, flatten=False):
     def convnet():
-        if dropout_rate: 
-            layers = []
-            for size in stack:
-                layers.append(tfkl.Conv2D(size, kernel_size=kernel_size, strides=strides, activation=activation))
-                layers.append(tfkl.Dropout(dropout_rate))
-            layers = layers[:-1]
-        else:
-            layers = [tfkl.Conv2D(size, kernel_size=kernel_size, strides=strides, activation=activation) for size in stack]
+       
+        layers = []
+        for size in stack:
+            layers.append(tfkl.Conv2D(size, kernel_size=kernel_size, strides=strides, activation=activation))
+            if dropout_rate: layers.append(tfkl.Dropout(dropout_rate))
+            layer.append(tfkl.MaxPool2D(pool_size=(2, 2)))
+        if dropout_rate: layers = layers[:-2]
+        else: layers = layers[:-1]
         if flatten: layers = layers + [tfkl.Flatten()]
         return tfk.Sequential(layers)
     return convnet 
 
 def init_convtransposenet(stack, kernel_size=5, strides=(2, 2), activation='relu', dropout_rate=None, flatten=False):
     def convtransposenet():
-        if dropout_rate: 
-            layers = []
-            for size in stack:
-                layers.append(tfkl.Conv2DTranspose(size, kernel_size=kernel_size, strides=strides, activation=activation))
-                layers.append(tfkl.Dropout(dropout_rate))
-            layers = layers[:-1]
-        else:
-            layers = [tfkl.Conv2DTranspose(size, kernel_size=kernel_size, strides=strides, activation=activation) for size in stack]
+        layers = []
+        for size in stack:
+            layers.append(tfkl.Conv2D(size, kernel_size=kernel_size, strides=strides, activation=activation))
+            if dropout_rate: layers.append(tfkl.Dropout(dropout_rate))
+            layer.append(tfkl.BatchNormalization())
+        if dropout_rate: layers = layers[:-2]
+        else: layers = layers[:-1]
         if flatten: layers = layers + [tfkl.Flatten()]
         return tfk.Sequential(layers)
     return convtransposenet
