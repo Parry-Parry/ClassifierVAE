0ebcf3e12f3b610559b3a61a6940f62dd40d204f
Andrew Parry <2393265p@student.gla.ac.uk>
2022-10-25 15:16:56 +0100

    Checking log_prob p_y

--- ClassifierVAE/utils.py
+++ ClassifierVAE/utils.py
@@ -53,7 +53,6 @@ Computes the latent fixed prior over the logits of y
 
 def compute_py(logits_y, n_class, tau): 
     logits_py = tf.ones_like(logits_y) * 1./n_class 
-    print(logits_py)
     return tfd.RelaxedOneHotCategorical(tau, logits=logits_py)
 
 '''
@@ -63,8 +62,8 @@ Initializes multitask loss with the sum taken over ensemble components
 def init_loss(multihead=False):
     cce = tfk.losses.CategoricalCrossentropy()
     def ensemble_loss(y_true, x_true, output):
+        print(output.p_y.log_prob(output.gen_y))
         qp_pairs = [q_y.log_prob(output.gen_y) - output.p_y.log_prob(output.gen_y) for q_y in output.q_y]
-        print(qp_pairs)
         KL = tf.reduce_sum([tf.reduce_sum(qp, 1) for qp in qp_pairs], axis=0, name="Sum of KL over Prior Distribution and Learned Distributions")
 
         intermediate = tfm.reduce_sum(tf.map_fn(lambda x : cce(y_true, x), elems=output.y_pred), axis=0, name="Sum of CE over Generated Preds")
